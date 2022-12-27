df17ec67978524da795f32a06b78833e711ecc35
shaghayegh momeni <shghygh.momeni18@gmail.com>
2022-12-27 14:40:53 +0330

    fix: categories added to dom in js file

--- src/scripts/homepage.js
+++ src/scripts/homepage.js
@@ -3,6 +3,8 @@ console.log(storageUserInfo);
 const picField = document.getElementById("pic-field");
 const nameField = document.getElementById("name-field");
 
+const categoriesField = document.getElementById("categories-section");
+
 // App bar
 function showUserInfo(name, lastname, picture) {
   console.log(picture);
@@ -18,3 +20,31 @@ showUserInfo(
 //search
 
 //categories
+function addCategoryToDOM() {
+  const singleCategoryDiv = document.createElement("div");
+  singleCategoryDiv.classList.add(
+    "flex",
+    "flex-col",
+    "justify-center",
+    "items-center"
+  );
+  categoriesField.append(singleCategoryDiv);
+
+  const categoryImg = document.createComment("img");
+  categoryImg.classList.add(
+    "w-[60px]",
+    "h-[60px]",
+    "bg-[#ECECEC]",
+    "rounded-full",
+    "p-4",
+    "mb-2"
+  );
+  // categoryImg.src = category.logo;
+  singleCategoryDiv.append(categoryImg);
+
+  const categoryName = document.createElement("p");
+  categoryName.classList.add("font-semibold");
+  // categoryName.innerText = category.name;
+  singleCategoryDiv.append(categoryName);
+}
+addCategoryToDOM();
