69832a637873382b18a33d6dcbf98adf9627d467
shaghayegh momeni <shghygh.momeni18@gmail.com>
2022-12-28 10:53:14 +0330

    fix:insert more box in category section of homepage

--- src/scripts/homepage.js
+++ src/scripts/homepage.js
@@ -2,12 +2,12 @@ const API_URL = "http://localhost:3000";
 const storageUserInfo = JSON.parse(localStorage.getItem("user"));
 const picField = document.getElementById("pic-field");
 const nameField = document.getElementById("name-field");
+let allCategoryData;
 
 const categoriesField = document.getElementById("categories-section");
 
 // App bar
 function showUserInfo(name, lastname, picture) {
-  console.log(picture);
   picField.src = picture;
   nameField.innerText = `${name} ${lastname}`;
 }
@@ -22,19 +22,23 @@ showUserInfo(
 //categories
 const getCategoriesList = async () => {
   try {
-    const res = await fetch(`${API_URL}/doctors`);
+    const res = await fetch(`${API_URL}/categories`);
     const data = await res.json();
-    data.forEach((item) => {
-      addDoctorsToDOM(item);
-    });
-    console.log(data);
-  } catch (e) {
-    console.log(e);
+    if (data.length <= 8) {
+      data.forEach((item) => {
+        addCategoryToDOM(item);
+      });
+    } else {
+      addFullCategoryToDOM(data);
+    }
+  } catch {
+    console.log("404 Page");
   }
 };
-getDoctorsList();
+getCategoriesList();
 
-function addCategoryToDOM() {
+function addCategoryToDOM(item) {
+  const logo = item.logo;
   const singleCategoryDiv = document.createElement("div");
   singleCategoryDiv.classList.add(
     "flex",
@@ -44,7 +48,7 @@ function addCategoryToDOM() {
   );
   categoriesField.append(singleCategoryDiv);
 
-  const categoryImg = document.createComment("img");
+  const categoryImg = document.createElement("img");
   categoryImg.classList.add(
     "w-[60px]",
     "h-[60px]",
@@ -53,12 +57,51 @@ function addCategoryToDOM() {
     "p-4",
     "mb-2"
   );
-  // categoryImg.src = category.logo;
+  categoryImg.src = logo;
   singleCategoryDiv.append(categoryImg);
 
   const categoryName = document.createElement("p");
-  categoryName.classList.add("font-semibold");
-  // categoryName.innerText = category.name;
+  categoryName.classList.add("font-semibold", "truncate", "w-13");
+  categoryName.innerText = item.brandName;
   singleCategoryDiv.append(categoryName);
 }
-addCategoryToDOM();
+
+function addFullCategoryToDOM(data) {
+  CategoryLimitSize = data.length;
+  allCategoryData = data;
+  console.log(allCategoryData);
+  for (let i = 0; i < 7; i++) {
+    addCategoryToDOM(allCategoryData[i]);
+  }
+  //insert more icon for categorires
+  const singleMoreDiv = document.createElement("div");
+  singleMoreDiv.classList.add(
+    "flex",
+    "flex-col",
+    "justify-center",
+    "items-center"
+  );
+  categoriesField.append(singleMoreDiv);
+  const moreImg = document.createElement("img");
+  moreImg.classList.add(
+    "w-[60px]",
+    "h-[60px]",
+    "bg-[#ECECEC]",
+    "rounded-full",
+    "p-4",
+    "mb-2"
+  );
+  moreImg.src = "../icons/more-icon.svg";
+  singleMoreDiv.append(moreImg);
+  const moreTitle = document.createElement("p");
+  moreTitle.classList.add("font-semibold", "truncate", "w-13");
+  moreTitle.innerText = "More ...";
+  singleMoreDiv.append(moreTitle);
+
+  singleMoreDiv.addEventListener("click", () => {
+    singleMoreDiv.classList.add("hidden");
+    for (let i = 7; i < CategoryLimitSize; i++) {
+      addCategoryToDOM(allCategoryData[i]);
+    }
+  });
+}
