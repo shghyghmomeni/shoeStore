75eb958998ffb3c4e86a75301acd6c6b90de263d
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-01-03 00:17:43 +0330

    fix: completed orders added in order page

--- src/scripts/peyment.js
+++ src/scripts/peyment.js
@@ -4,6 +4,12 @@ backIcon.addEventListener("click", showPrePage);
 const applyBtn = document.getElementById("apply-btn");
 applyBtn.addEventListener("click", goTocheckoutPage);
 
+let storageUserInfo = JSON.parse(localStorage.getItem("user"))
+  ? JSON.parse(localStorage.getItem("user"))
+  : [];
+let activeOrders = storageUserInfo.orders.active;
+let ComletedOrders = storageUserInfo.orders.completed;
+
 //back app bar
 function showPrePage() {
   history.back();
@@ -11,5 +17,14 @@ function showPrePage() {
 
 //apply btn
 function goTocheckoutPage() {
+  activeOrders.forEach((element) => {
+    ComletedOrders.push(element);
+  });
+  activeOrders.length = 0;
+
+  let storageUserInfo = localStorage.setItem(
+    "user",
+    JSON.stringify(storageUserInfo)
+  );
   window.open("../pages/orderSuccessful.html", "_self");
 }
