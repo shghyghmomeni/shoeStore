bf4a45f81f7bf40140668d85eb5816536e7b1f57
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-01-02 12:17:52 +0330

    fix: tabs toggles a class when click

---
+++ src/scripts/orders.js
@@ -0,0 +1,11 @@
+let activeTab = document.getElementById("active-tab");
+let completeTab = document.getElementById("complete-tab");
+
+function showActiveProducts() {
+  activeTab.classList.toggle("border-b-4");
+  completeTab.classList.toggle("border-b-4");
+}
+function showCompleteProducts() {
+  activeTab.classList.toggle("border-b-4");
+  completeTab.classList.toggle("border-b-4");
+}
