096fe226bd38dcf1f92ce75eb075c420702c292d
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-01-02 18:25:24 +0330

    fix: show default address and user infotmation

---
+++ src/scripts/profile.js
@@ -0,0 +1,33 @@
+let userInfo = JSON.parse(localStorage.getItem("user"));
+const userName = document.getElementById("user-name");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+// console.log(userInfo);
+let defaultAdd = "";
+function showUserInformations(userInfo) {
+  userName.innerText = `${userInfo.name} ${userInfo.lastname}`;
+  email.innerText = userInfo.email;
+  console.log(userInfo.address);
+
+  userInfo.address.filter((item) => {
+    if (item.defaultAddress == true) {
+      defaultAdd = item.address;
+    }
+  });
+  address.innerText = defaultAdd;
+
+  console.log(defaultAdd);
+
+  // let defaultAddress = () => {
+  //   address.innerText = userInfo.address;
+  // };
+}
+showUserInformations(userInfo);
+
+function goToManageAddressPage() {
+  window.open("../manageAddress.html", "_self");
+}
+
+function goToWishlistPage() {
+  window.open("../wishlist.html", "_self");
+}
